405342563f8172cb8af54f613d282c1e6cf21144
Clayton Ward <clayton@flaresoftware.com>
2023-09-11 17:39:43 -0700

    Create http-request.py

---
+++ python/http-request.py
@@ -0,0 +1,36 @@
+"""
+Boilerplate for quickly writing python http request scripts.
+
+Install requests via pip:
+python3 -m pip install requests
+
+usage: python3 http-request.py
+
+output:
+Response from: GET https://example.com/
+status: 200
+content type: text/html; charset=UTF-8
+encoding: UTF-8
+"""
+
+import requests
+
+URL = 'https://example.com/'
+
+def main():
+    r = requests.get(URL)
+
+    print(f"""
+Response from: GET {URL}
+status: {r.status_code}
+content type: {r.headers['content-type']}
+encoding: {r.encoding}
+        """)
+    
+    # response body:
+    # text: {r.text}
+    # json: r.json()
+
+
+if __name__ == '__main__':
+    main()
